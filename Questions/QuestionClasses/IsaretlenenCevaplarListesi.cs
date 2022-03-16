bff14be71901110f603cb697f6d9324ff637835d
mustafa uçar <mustafa_asus@hotmail.com>
2022-03-16 21:20:36 +0300

    Add selected answer Id list

--- Questions/QuestionClasses/IsaretlenenCevaplarListesi.cs
+++ Questions/QuestionClasses/IsaretlenenCevaplarListesi.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections.Generic;
+using MongoDB.Bson;
 
 namespace Question_Answer.Questions.QuestionForms;
 
 class IsaretlenenCevaplarListesi
 {
-    public List<IsaretlenenCevapClass> İIsaretlenenCevapClasses;
+    public List<ObjectId> SelectedAnswerIdList;
 
     public IsaretlenenCevaplarListesi()
     {
-        İIsaretlenenCevapClasses = new List<IsaretlenenCevapClass>();
+        SelectedAnswerIdList = new List<ObjectId>();
     }
 }
