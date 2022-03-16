a7ef98ad6cc2c807774ccb735c02547b9dec4750
mustafa uçar <mustafa_asus@hotmail.com>
2022-03-17 01:54:25 +0300

    Add Quiz form for students quiz

---
+++ Questions/QuestionForms/QuizFrom.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace Question_Answer.Questions.QuestionForms
+{
+    partial class QuizFrom
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Lbl_Soru = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.radioA = new System.Windows.Forms.RadioButton();
+            this.radioB = new System.Windows.Forms.RadioButton();
+            this.radioC = new System.Windows.Forms.RadioButton();
+            this.radioD = new System.Windows.Forms.RadioButton();
+            this.radioE = new System.Windows.Forms.RadioButton();
+            this.btn_DigerSoru = new System.Windows.Forms.Button();
+            this.btn_Onayla = new System.Windows.Forms.Button();
+            this.btn_OncekiSoru = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.OptionsPanel = new System.Windows.Forms.Panel();
+            this.optikPanel = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.OptionsPanel.SuspendLayout();
+            this.optikPanel.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Lbl_Soru
+            // 
+            this.Lbl_Soru.AutoSize = true;
+            this.Lbl_Soru.BackColor = System.Drawing.Color.White;
+            this.Lbl_Soru.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Lbl_Soru.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Lbl_Soru.Location = new System.Drawing.Point(29, 330);
+            this.Lbl_Soru.MaximumSize = new System.Drawing.Size(600, 199);
+            this.Lbl_Soru.Name = "Lbl_Soru";
+            this.Lbl_Soru.Size = new System.Drawing.Size(2, 27);
+            this.Lbl_Soru.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(798, 303);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // radioA
+            // 
+            this.radioA.BackColor = System.Drawing.Color.White;
+            this.radioA.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.radioA.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioA.Location = new System.Drawing.Point(0, 8);
+            this.radioA.Margin = new System.Windows.Forms.Padding(0);
+            this.radioA.MaximumSize = new System.Drawing.Size(700, 123);
+            this.radioA.Name = "radioA";
+            this.radioA.Size = new System.Drawing.Size(700, 62);
+            this.radioA.TabIndex = 2;
+            this.radioA.Text = "s";
+            this.radioA.UseVisualStyleBackColor = false;
+            this.radioA.Visible = false;
+            this.radioA.CheckedChanged += new System.EventHandler(this.radioBtnsCheckedChanged);
+            // 
+            // radioB
+            // 
+            this.radioB.BackColor = System.Drawing.Color.White;
+            this.radioB.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.radioB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioB.Location = new System.Drawing.Point(0, 256);
+            this.radioB.Margin = new System.Windows.Forms.Padding(0);
+            this.radioB.MaximumSize = new System.Drawing.Size(700, 123);
+            this.radioB.Name = "radioB";
+            this.radioB.Size = new System.Drawing.Size(700, 62);
+            this.radioB.TabIndex = 3;
+            this.radioB.Text = "s";
+            this.radioB.UseVisualStyleBackColor = false;
+            this.radioB.Visible = false;
+            // 
+            // radioC
+            // 
+            this.radioC.BackColor = System.Drawing.Color.White;
+            this.radioC.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.radioC.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioC.Location = new System.Drawing.Point(0, 194);
+            this.radioC.Margin = new System.Windows.Forms.Padding(0);
+            this.radioC.MaximumSize = new System.Drawing.Size(700, 123);
+            this.radioC.Name = "radioC";
+            this.radioC.Size = new System.Drawing.Size(700, 62);
+            this.radioC.TabIndex = 4;
+            this.radioC.Text = "s";
+            this.radioC.UseVisualStyleBackColor = false;
+            this.radioC.Visible = false;
+            // 
+            // radioD
+            // 
+            this.radioD.BackColor = System.Drawing.Color.White;
+            this.radioD.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.radioD.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioD.Location = new System.Drawing.Point(0, 132);
+            this.radioD.Margin = new System.Windows.Forms.Padding(0);
+            this.radioD.MaximumSize = new System.Drawing.Size(700, 123);
+            this.radioD.Name = "radioD";
+            this.radioD.Size = new System.Drawing.Size(700, 62);
+            this.radioD.TabIndex = 5;
+            this.radioD.Text = "s";
+            this.radioD.UseVisualStyleBackColor = false;
+            this.radioD.Visible = false;
+            // 
+            // radioE
+            // 
+            this.radioE.BackColor = System.Drawing.Color.White;
+            this.radioE.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.radioE.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioE.Location = new System.Drawing.Point(0, 70);
+            this.radioE.Margin = new System.Windows.Forms.Padding(0);
+            this.radioE.MaximumSize = new System.Drawing.Size(700, 123);
+            this.radioE.Name = "radioE";
+            this.radioE.Size = new System.Drawing.Size(700, 62);
+            this.radioE.TabIndex = 6;
+            this.radioE.Text = "s";
+            this.radioE.UseVisualStyleBackColor = false;
+            this.radioE.Visible = false;
+            // 
+            // btn_DigerSoru
+            // 
+            this.btn_DigerSoru.BackColor = System.Drawing.Color.White;
+            this.btn_DigerSoru.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_DigerSoru.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_DigerSoru.Location = new System.Drawing.Point(739, 16);
+            this.btn_DigerSoru.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_DigerSoru.Name = "btn_DigerSoru";
+            this.btn_DigerSoru.Size = new System.Drawing.Size(45, 38);
+            this.btn_DigerSoru.TabIndex = 12;
+            this.btn_DigerSoru.Text = ">";
+            this.btn_DigerSoru.UseVisualStyleBackColor = false;
+            this.btn_DigerSoru.Click += new System.EventHandler(this.btn_DigerSoru_Click);
+            // 
+            // btn_Onayla
+            // 
+            this.btn_Onayla.BackColor = System.Drawing.Color.Firebrick;
+            this.btn_Onayla.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btn_Onayla.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_Onayla.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_Onayla.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_Onayla.ForeColor = System.Drawing.Color.White;
+            this.btn_Onayla.Location = new System.Drawing.Point(0, 0);
+            this.btn_Onayla.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_Onayla.Name = "btn_Onayla";
+            this.btn_Onayla.Size = new System.Drawing.Size(351, 64);
+            this.btn_Onayla.TabIndex = 13;
+            this.btn_Onayla.Text = "Testi bitir";
+            this.btn_Onayla.UseVisualStyleBackColor = true;
+            this.btn_Onayla.Click += new System.EventHandler(this.btn_Onayla_Click);
+            // 
+            // btn_OncekiSoru
+            // 
+            this.btn_OncekiSoru.BackColor = System.Drawing.Color.White;
+            this.btn_OncekiSoru.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_OncekiSoru.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_OncekiSoru.Location = new System.Drawing.Point(679, 16);
+            this.btn_OncekiSoru.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_OncekiSoru.Name = "btn_OncekiSoru";
+            this.btn_OncekiSoru.Size = new System.Drawing.Size(45, 38);
+            this.btn_OncekiSoru.TabIndex = 14;
+            this.btn_OncekiSoru.Text = "<";
+            this.btn_OncekiSoru.UseVisualStyleBackColor = false;
+            this.btn_OncekiSoru.Click += new System.EventHandler(this.btn_OncekiSoru_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.OptionsPanel);
+            this.panel1.Controls.Add(this.Lbl_Soru);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(798, 925);
+            this.panel1.TabIndex = 15;
+            // 
+            // OptionsPanel
+            // 
+            this.OptionsPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.OptionsPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.OptionsPanel.Controls.Add(this.radioA);
+            this.OptionsPanel.Controls.Add(this.radioE);
+            this.OptionsPanel.Controls.Add(this.radioD);
+            this.OptionsPanel.Controls.Add(this.radioC);
+            this.OptionsPanel.Controls.Add(this.radioB);
+            this.OptionsPanel.Location = new System.Drawing.Point(12, 499);
+            this.OptionsPanel.MaximumSize = new System.Drawing.Size(783, 320);
+            this.OptionsPanel.MinimumSize = new System.Drawing.Size(783, 320);
+            this.OptionsPanel.Name = "OptionsPanel";
+            this.OptionsPanel.Size = new System.Drawing.Size(783, 320);
+            this.OptionsPanel.TabIndex = 15;
+            this.OptionsPanel.Visible = false;
+            // 
+            // optikPanel
+            // 
+            this.optikPanel.AutoScroll = true;
+            this.optikPanel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.optikPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.optikPanel.Controls.Add(this.panel3);
+            this.optikPanel.Cursor = System.Windows.Forms.Cursors.Default;
+            this.optikPanel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.optikPanel.Location = new System.Drawing.Point(804, 0);
+            this.optikPanel.Name = "optikPanel";
+            this.optikPanel.Size = new System.Drawing.Size(353, 925);
+            this.optikPanel.TabIndex = 16;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.btn_Onayla);
+            this.panel3.Cursor = System.Windows.Forms.Cursors.Default;
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(0, 859);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(351, 64);
+            this.panel3.TabIndex = 14;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btn_OncekiSoru);
+            this.panel2.Controls.Add(this.btn_DigerSoru);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 860);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(798, 65);
+            this.panel2.TabIndex = 16;
+            // 
+            // QuestionGetForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1157, 925);
+            this.Controls.Add(this.optikPanel);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "QuestionGetForm";
+            this.Text = "QuestionGetForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.OptionsPanel.ResumeLayout(false);
+            this.optikPanel.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Lbl_Soru;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton radioA;
+        private System.Windows.Forms.RadioButton radioB;
+        private System.Windows.Forms.RadioButton radioC;
+        private System.Windows.Forms.RadioButton radioD;
+        private System.Windows.Forms.RadioButton radioE;
+        private System.Windows.Forms.Button btn_DigerSoru;
+        private System.Windows.Forms.Button btn_Onayla;
+        private System.Windows.Forms.Button btn_OncekiSoru;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel optikPanel;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel OptionsPanel;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
---
+++ Questions/QuestionForms/QuizFrom.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using MongoDB.Bson;
+using Question_Answer.user.StudentUserForm;
+using Question_Answer.user.UserClasses;
+
+namespace Question_Answer.Questions.QuestionForms
+{
+    public partial class QuizFrom : Form
+    {
+        public List<Question> RandomQuestionList;
+        public int SoruNum;
+        public Student student;
+        private int _thisQuestionTrueAnswerCount;
+        private int _thisQuestionFalseAnswerCount;
+        private Question randomQuestion;
+        private RadioButton radioCevap;
+        private IsaretlenenCevaplarListesi _studentAnswerlist;
+
+        public QuizFrom(Student student)
+        {
+            InitializeComponent();
+            RandomQuestionList = new Question().Rastgele10SoruGetir<Question>();
+            SoruNum = 0;
+            _thisQuestionTrueAnswerCount = 0;
+            _thisQuestionFalseAnswerCount = 0;
+
+            _studentAnswerlist = new IsaretlenenCevaplarListesi();
+            SorularıGetir(SoruNum);
+            OptionsPanel.Visible = true;
+            this.student = student;
+
+
+            // yeni panel yan optik
+            for (int i = 0; i < RandomQuestionList.Count; i++)
+            {
+                var questionpanel = OptikIcinPanelOlustur(RandomQuestionList[i].QuestionId, i % 2 == 1 ? Color.Turquoise : Color.Aquamarine);
+                for (int j = 0; j < 5; j++)
+                {
+                    questionpanel.Controls.Add(OptikIcinRadioButtonOlustur(RandomQuestionList[i].Answers[j], j));
+                }
+            }
+
+            //////////////////////////
+
+
+        }
+
+        private Panel OptikIcinPanelOlustur(ObjectId questionId, Color color)//Optir bar için panel
+        {
+            var pnl = new Panel
+            {
+                Width = optikPanel.Width,
+                Height = 84,
+                BorderStyle = BorderStyle.FixedSingle,
+                BackColor = color,
+                Name = questionId.ToString()
+            };
+
+            pnl.Top += (optikPanel.Controls.Count - 1) * 84;
+            optikPanel.Controls.Add(pnl);
+            return pnl;
+
+        }
+
+        private RadioButton OptikIcinRadioButtonOlustur(Answers opAnswers, int j)
+        {
+            var rBtn = new RadioButton
+            {
+                CheckAlign = ContentAlignment.MiddleCenter,
+                Dock = DockStyle.Right,
+                Location = new Point(j * optikPanel.Width / 5, 0),
+                Size = new Size(optikPanel.Width / 5, 82),
+                Name = "optik_" + opAnswers.AnswerId,
+                TabStop = true,
+                TextAlign = ContentAlignment.MiddleCenter,
+                UseVisualStyleBackColor = true,
+            };
+            rBtn.CheckedChanged += radioBtnsCheckedChanged;
+
+            return rBtn;
+        }
+
+        private List<RadioButton> CevapSecenekleriOlustur(List<Answers> answersList)
+        {
+            var RadioButtonList = new List<RadioButton>();
+            foreach (var answer in answersList)
+            {
+                radioCevap = new RadioButton
+                {
+                    BackColor = Color.White,
+                    Dock = DockStyle.Bottom,
+                    Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(162))),
+                    Location = new Point(0, 6),
+                    Margin = new Padding(0),
+                    MaximumSize = new Size(700, 123),
+                    Name = answer.AnswerId.ToString(),
+                    Size = new Size(700, 50),
+                    Text = answer.AnswerText,
+                    TabIndex = 2,
+                    UseVisualStyleBackColor = false,
+                };
+                radioCevap.CheckedChanged += radioBtnsCheckedChanged;
+                OptionsPanel.Controls.Add(radioCevap);
+                RadioButtonList.Add(radioCevap);
+            }
+
+            return RadioButtonList;
+
+        }
+
+        private ObjectId _trueAnswerId;
+        private void btn_DigerSoru_Click(object sender, EventArgs e)
+        {
+            if (SoruNum < RandomQuestionList.Count - 1)
+                SoruNum++;
+            SorularıGetir(SoruNum);
+
+        }
+        private void btn_OncekiSoru_Click(object sender, EventArgs e)
+        {
+            if (SoruNum > 0)
+                SoruNum--;
+            SorularıGetir(SoruNum);
+
+        }
+
+        private void SorularıGetir(int index)
+        {
+
+
+            randomQuestion = RandomQuestionList[index];
+            OptionsPanel.Controls.Clear();
+            foreach (var radioButton in CevapSecenekleriOlustur(randomQuestion.Answers))
+                OptionsPanel.Controls.Add(radioButton);
+
+            foreach (var radioButton in OptionsPanel.Controls.OfType<RadioButton>())
+            {
+                foreach (var randomQuestionAnswer in _studentAnswerlist.SelectedAnswerIdList)
+                {
+                    if (randomQuestionAnswer.ToString() == radioButton.Name && !radioButton.Checked)
+                    {
+                        radioButton.Checked = true;
+                    }
+                }
+            }
+
+
+            Lbl_Soru.Text = (index + 1) + " ) " + randomQuestion.QuestionText;
+
+            pictureBox1.ImageLocation = randomQuestion.QuestionImage != null
+                ? randomQuestion.QuestionImagesUploadLocation + @"\" + randomQuestion.QuestionImage
+                : null;
+        }
+
+
+        private void btn_Onayla_Click(object sender, EventArgs e)
+        {
+
+            for (var index = 0; index < RandomQuestionList.Count; index++)// sorulan soru for'u
+            {
+                var questionListElement = RandomQuestionList[index];
+                foreach (var SelectedAnswerIdListElement in _studentAnswerlist.SelectedAnswerIdList) // öğrencinin işaretlediği cevaplar
+                {
+                    foreach (var questionListAnswerElement in questionListElement.Answers)
+                    {
+                        if (SelectedAnswerIdListElement == questionListAnswerElement.AnswerId)
+                        {
+                            foreach (Control optikPanelControl in optikPanel.Controls)
+                            {
+                                if (optikPanelControl.Name == questionListElement.QuestionId.ToString())
+                                {
+                                    if (questionListAnswerElement.TrueOrFalse)
+                                    {
+                                        optikPanelControl.BackColor = Color.Chartreuse;
+                                        _thisQuestionTrueAnswerCount++;
+                                        student.AnsweredQuestionsList.Add(new AnsweredQuestions
+                                        {
+                                            TrueOrFalse = true,
+                                            AnsweredQuestionIds = questionListElement.QuestionId,
+                                            AnsweredQuestionDate = DateTime.Now
+
+                                        });
+                                    }
+                                    else
+                                    {
+                                        optikPanelControl.BackColor = Color.Red;
+                                        _thisQuestionFalseAnswerCount++;
+                                        student.AnsweredQuestionsList.Add(new AnsweredQuestions
+                                        {
+                                            TrueOrFalse = false,
+                                            AnsweredQuestionIds = questionListElement.QuestionId,
+                                            AnsweredQuestionDate = DateTime.Now
+                                        });
+                                    }
+                                }
+                            }
+
+                        }
+                    }
+                }
+            }
+            student.TrueCount += _thisQuestionTrueAnswerCount;
+            student.FalseCount += _thisQuestionFalseAnswerCount;
+            student.Save();
+            MessageBox.Show("Bu testin " +
+                            $"\nDoğru cevap sayısı : {_thisQuestionTrueAnswerCount}" +
+                            $"\nYanlış cevap sayısı : {_thisQuestionFalseAnswerCount}");
+            optikPanel.Enabled = false;
+            OptionsPanel.Enabled = false;
+            StudentDashboardForm dashboardForm = new StudentDashboardForm(student);
+            dashboardForm.Show();
+            //btn_Onayla.Enabled = false;
+
+        }
+
+        public void radioBtnsCheckedChanged(object sender, EventArgs e)
+        {
+            var SelectedRadioButton = sender as RadioButton;
+
+            if (SelectedRadioButton is { Checked: false })
+            {
+                _studentAnswerlist.SelectedAnswerIdList.Remove(SelectedRadioButton.Text == ""
+                    ? new ObjectId(SelectedRadioButton.Name.Split('_')[1])
+                    : new ObjectId(SelectedRadioButton.Name));
+            }
+            if (SelectedRadioButton is { Checked: true })
+            {
+                var ısaretlenenCevapId = (SelectedRadioButton.Text == ""
+                    ? new ObjectId(SelectedRadioButton.Name.Split('_')[1])
+                    : new ObjectId(SelectedRadioButton.Name));
+
+                if (_studentAnswerlist.SelectedAnswerIdList.Count == 0)
+                {
+                    _studentAnswerlist.SelectedAnswerIdList.Add(ısaretlenenCevapId);
+                }
+
+                //Aynı answerid eklememek için döngü
+                var confirmation = true;
+                for (var ındex = 0; ındex < _studentAnswerlist.SelectedAnswerIdList.Count; ındex++)
+                {
+                    var objectId = _studentAnswerlist.SelectedAnswerIdList[ındex];
+                    if (ısaretlenenCevapId == objectId) // son eleman
+                    {
+                        confirmation = false;
+                        break;
+                    }
+                }
+
+                if (confirmation)
+                {
+                    _studentAnswerlist.SelectedAnswerIdList.Add(ısaretlenenCevapId);
+
+                }
+
+                foreach (var panel in optikPanel.Controls.OfType<Panel>())//optik panel
+                {
+                    if (panel.Name == randomQuestion.QuestionId.ToString())//optik panel içindeki soru id ile görüntülenen soru id karşılaştırma
+                    {
+                        foreach (var radio in panel.Controls.OfType<RadioButton>()) // optik panel > soru id panel > radiobuttonlar
+                        {
+                            if (radio.Name == "optik_" + ısaretlenenCevapId)
+                            {
+                                radio.Checked = true;
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
