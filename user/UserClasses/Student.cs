a9b8fdd578ec62486740c2207ccd30c101f3bffb
mustafa uçar <mustafa_asus@hotmail.com>
2022-03-18 01:34:01 +0300

    fix student

--- user/UserClasses/Student.cs
+++ user/UserClasses/Student.cs
@@ -6,7 +6,7 @@ using Question_Answer.user.UserClass;
 
 namespace Question_Answer.user.UserClasses
 {
-    public class Student: User
+    public class Student : User
     {
         [BsonElement("TrueCount")]
         public int TrueCount { get; set; }
@@ -27,7 +27,7 @@ namespace Question_Answer.user.UserClasses
 
         public void Save()
         {
-            mongoDB.UpdateRecord("Students",this.UserId,this);
+            mongoDB.UpdateRecord("Students", this.UserId, this);
         }
     }
 }
