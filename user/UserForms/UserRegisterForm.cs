c457c0ed26b7c52b66d1c96e7fcbeab820b8945a
mustafa uçar <mustafa_asus@hotmail.com>
2022-03-18 02:30:38 +0300

    Add register for teacher and update form to kryptonFrom

--- user/UserForms/UserRegisterForm.cs
+++ user/UserForms/UserRegisterForm.cs
@@ -3,83 +3,105 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
+using ComponentFactory.Krypton.Toolkit;
 using MongoDB.Bson;
+using Question_Answer.Questions;
 using Question_Answer.user.UserClass;
 using Question_Answer.user.UserClasses;
 
+
 namespace Question_Answer.user.UserForms
+
 {
-    public partial class UserRegisterForm : Form
+    public partial class UserRegisterForm : KryptonForm
     {
         public UserRegisterForm()
         {
             InitializeComponent();
-            rBtn_Ogrenci.Checked = true;
         }
 
         private void btn_KayitOl_Click(object sender, EventArgs e)
         {
             var db = new Database.MongoDB();
 
-            if (db.LoadRecords<User>("Students").All(kullanici => kullanici.Username != textKullaniciAdi.Text))
+            foreach (var kryptonTextBox in kryptonPanel1.Controls.OfType<KryptonTextBox>())
             {
-                var Role = rBtn_Ogrenci.Checked
-                    ? rBtn_Ogrenci.Text
-                    : (rBtn_Ogretmen.Checked ? rBtn_Ogretmen.Text : rBtn_Admin.Text);
-                var AnsweredQuestionsList = new List<AnsweredQuestions>();
-
-                AnsweredQuestionsList.Add(new AnsweredQuestions
+                if (kryptonTextBox.Text.Length == 0)
                 {
-                    TrueOrFalse = true,
-                    AnsweredQuestionIds = new ObjectId("622d5148632fadd4f8efb3bd")
-                });
-                if (Role == "Öğrenci")
-                {
-                    Student student = new Student
-                    {
-                        Username = textKullaniciAdi.Text,
-                        Password = textParola.Text,
-                        Name = textAd.Text,
-                        Lastname = textSoyad.Text,
-                        Email = textMail.Text,
-                        Role = "Öğrenci",
-                        TrueCount = 1,
-                        FalseCount = 0,
-                        AnsweredQuestionsList = AnsweredQuestionsList
-
-                    };
-                    db.InsertRecord("Students", student);
-                    MessageBox.Show($"Öğrenci Başarıyla kayıt oldu hoş geldiniz {student.Name}");
-
+                    MessageBox.Show(@"Lütfen tüm boşlukları doldurun");
+                    return;
                 }
 
+                if (text_Parola.Text.Length < 5)
+                {
+                    MessageBox.Show(@"Parola uzunluğu en az 5 olmalı");
+                    return;
+                }
+            }
 
-                //var user = new User
-                //{
-                //    Username = textKullaniciAdi.Text,
-                //    Password = textParola.Text,
-                //    Name = textAd.Text,
-                //    Lastname = textSoyad.Text,
-                //    Email = textMail.Text,
-                //    Role = rBtn_Ogrenci.Checked ? rBtn_Ogrenci.Text : (rBtn_Ogretmen.Checked ? rBtn_Ogretmen.Text : rBtn_Admin.Text)
-                //};
+            if (new User().UsersUsernameAndEmailCheck(text_KullaniciAdi.Text))
+            {
+                var Role = rBtn_ogrenci.Checked
+                    ? rBtn_ogrenci.Text
+                    : rBtn_ogretmen.Text;
+                var answeredQuestionsList = new List<AnsweredQuestions>();
+                var addedQuestionsIdList = new List<ObjectId>();
+                
+                switch (Role)
+                {
+                    case "Öğrenci":
+                    {
+                        var student = new Student
+                        {
+                            Username = text_KullaniciAdi.Text,
+                            Password = text_Parola.Text,
+                            Name = text_Ad.Text,
+                            Lastname = text_Soyad.Text,
+                            Email = text_Eposta.Text,
+                            Role = "Öğrenci",
+                            TrueCount = 0,
+                            FalseCount = 0,
+                            AnsweredQuestionsList = answeredQuestionsList
 
-                //db.InsertRecord("User", user);
-                //MessageBox.Show($"Kullanıcı Başarıyla kayıt oldu hoş geldiniz {user.Name}");
+                        };
+                        db.InsertRecord("Students", student);
+                        MessageBox.Show($"Öğrenci başarıyla kayıt oldu hoş geldiniz {student.Name}");
+                        break;
+                    }
+                    case "Öğretmen":
+                        var teacher = new Teacher
+                        {
+                            Username = text_KullaniciAdi.Text,
+                            Password = text_Parola.Text,
+                            Name = text_Ad.Text,
+                            Lastname = text_Soyad.Text,
+                            Email = text_Eposta.Text,
+                            Role = "Öğretmen",
+                            AddedQuestionCount = 0,
+                            AddedQuestionsIds = addedQuestionsIdList
+                        };
+                        db.InsertRecord("Teachers",teacher);
+                        MessageBox.Show($"Öğretmen başarıyla kayıt oldu hoş geldiniz {teacher.Name}");
+
+                        break;
+                }
             }
             else
             {
                 MessageBox.Show(@"Bu kullanıcı adı zaten kullanımda");
-                textKullaniciAdi.ForeColor = Color.Red;
-                textParola.Text = "";
+                text_KullaniciAdi.StateCommon.Content.Color1 = Color.Red;
+                text_Parola.Text = "";
             }
 
         }
 
         private void textKullaniciAdi_KeyPress(object sender, KeyPressEventArgs e)
         {
-            if (textKullaniciAdi.ForeColor == Color.Red) 
-                textKullaniciAdi.ForeColor = Color.Black;
+            if (text_KullaniciAdi.StateCommon.Content.Color1 == Color.Red)
+            {
+                text_KullaniciAdi.StateCommon.Content.Color1 = Color.Black;
+            }
         }
+
     }
 }
