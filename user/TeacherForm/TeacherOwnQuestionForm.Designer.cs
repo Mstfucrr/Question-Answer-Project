b09dc072f87b5f0b8444fc5e8c86043dca118acf
mustafa uçar <mustafa_asus@hotmail.com>
2022-03-20 01:03:10 +0300

    Add Teacher Own Question Form

---
+++ user/TeacherForm/TeacherOwnQuestionForm.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Question_Answer.user.TeacherForm
+{
+    partial class TeacherOwnQuestionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.kryptonPanel2 = new ComponentFactory.Krypton.Toolkit.KryptonPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Lbl_Cvp5 = new System.Windows.Forms.Label();
+            this.Lbl_Cvp4 = new System.Windows.Forms.Label();
+            this.Lbl_Cvp3 = new System.Windows.Forms.Label();
+            this.Lbl_Cvp2 = new System.Windows.Forms.Label();
+            this.Lbl_Cvp1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Lbl_Soru = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Lbl_AdminOnay = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel2)).BeginInit();
+            this.kryptonPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // kryptonPanel2
+            // 
+            this.kryptonPanel2.AutoSize = true;
+            this.kryptonPanel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.kryptonPanel2.Controls.Add(this.Lbl_AdminOnay);
+            this.kryptonPanel2.Controls.Add(this.label2);
+            this.kryptonPanel2.Controls.Add(this.Lbl_Soru);
+            this.kryptonPanel2.Controls.Add(this.pictureBox1);
+            this.kryptonPanel2.Controls.Add(this.Lbl_Cvp5);
+            this.kryptonPanel2.Controls.Add(this.Lbl_Cvp4);
+            this.kryptonPanel2.Controls.Add(this.Lbl_Cvp3);
+            this.kryptonPanel2.Controls.Add(this.Lbl_Cvp2);
+            this.kryptonPanel2.Controls.Add(this.Lbl_Cvp1);
+            this.kryptonPanel2.Controls.Add(this.label1);
+            this.kryptonPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.kryptonPanel2.Location = new System.Drawing.Point(0, 0);
+            this.kryptonPanel2.Name = "kryptonPanel2";
+            this.kryptonPanel2.PanelBackStyle = ComponentFactory.Krypton.Toolkit.PaletteBackStyle.TabOneNote;
+            this.kryptonPanel2.Size = new System.Drawing.Size(699, 808);
+            this.kryptonPanel2.StateCommon.Color1 = System.Drawing.SystemColors.ButtonShadow;
+            this.kryptonPanel2.StateCommon.Color2 = System.Drawing.Color.Aquamarine;
+            this.kryptonPanel2.StateCommon.ColorAngle = 60F;
+            this.kryptonPanel2.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Location = new System.Drawing.Point(74, 15);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(547, 202);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Lbl_Cvp5
+            // 
+            this.Lbl_Cvp5.BackColor = System.Drawing.Color.Transparent;
+            this.Lbl_Cvp5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Lbl_Cvp5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Lbl_Cvp5.Location = new System.Drawing.Point(74, 720);
+            this.Lbl_Cvp5.Name = "Lbl_Cvp5";
+            this.Lbl_Cvp5.Size = new System.Drawing.Size(547, 50);
+            this.Lbl_Cvp5.TabIndex = 6;
+            // 
+            // Lbl_Cvp4
+            // 
+            this.Lbl_Cvp4.BackColor = System.Drawing.Color.Transparent;
+            this.Lbl_Cvp4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Lbl_Cvp4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Lbl_Cvp4.Location = new System.Drawing.Point(74, 660);
+            this.Lbl_Cvp4.Name = "Lbl_Cvp4";
+            this.Lbl_Cvp4.Size = new System.Drawing.Size(547, 50);
+            this.Lbl_Cvp4.TabIndex = 5;
+            // 
+            // Lbl_Cvp3
+            // 
+            this.Lbl_Cvp3.BackColor = System.Drawing.Color.Transparent;
+            this.Lbl_Cvp3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Lbl_Cvp3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Lbl_Cvp3.Location = new System.Drawing.Point(74, 601);
+            this.Lbl_Cvp3.Name = "Lbl_Cvp3";
+            this.Lbl_Cvp3.Size = new System.Drawing.Size(547, 50);
+            this.Lbl_Cvp3.TabIndex = 4;
+            // 
+            // Lbl_Cvp2
+            // 
+            this.Lbl_Cvp2.BackColor = System.Drawing.Color.Transparent;
+            this.Lbl_Cvp2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Lbl_Cvp2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Lbl_Cvp2.Location = new System.Drawing.Point(74, 539);
+            this.Lbl_Cvp2.Name = "Lbl_Cvp2";
+            this.Lbl_Cvp2.Size = new System.Drawing.Size(547, 50);
+            this.Lbl_Cvp2.TabIndex = 3;
+            // 
+            // Lbl_Cvp1
+            // 
+            this.Lbl_Cvp1.BackColor = System.Drawing.Color.Transparent;
+            this.Lbl_Cvp1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Lbl_Cvp1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Lbl_Cvp1.Location = new System.Drawing.Point(74, 478);
+            this.Lbl_Cvp1.Name = "Lbl_Cvp1";
+            this.Lbl_Cvp1.Size = new System.Drawing.Size(547, 50);
+            this.Lbl_Cvp1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(70, 220);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Soru :";
+            // 
+            // Lbl_Soru
+            // 
+            this.Lbl_Soru.BackColor = System.Drawing.Color.White;
+            this.Lbl_Soru.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Lbl_Soru.Location = new System.Drawing.Point(74, 245);
+            this.Lbl_Soru.Name = "Lbl_Soru";
+            this.Lbl_Soru.ReadOnly = true;
+            this.Lbl_Soru.Size = new System.Drawing.Size(547, 219);
+            this.Lbl_Soru.TabIndex = 8;
+            this.Lbl_Soru.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(401, 774);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 25);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Admin onayı :";
+            // 
+            // Lbl_AdminOnay
+            // 
+            this.Lbl_AdminOnay.AutoSize = true;
+            this.Lbl_AdminOnay.BackColor = System.Drawing.Color.Transparent;
+            this.Lbl_AdminOnay.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Lbl_AdminOnay.Location = new System.Drawing.Point(538, 774);
+            this.Lbl_AdminOnay.Name = "Lbl_AdminOnay";
+            this.Lbl_AdminOnay.Size = new System.Drawing.Size(0, 25);
+            this.Lbl_AdminOnay.TabIndex = 10;
+            // 
+            // TeacherOwnQuestionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(699, 808);
+            this.Controls.Add(this.kryptonPanel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "TeacherOwnQuestionForm";
+            this.Text = "TeacherOwnQuestionForm";
+            this.Load += new System.EventHandler(this.TeacherOwnQuestionForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel2)).EndInit();
+            this.kryptonPanel2.ResumeLayout(false);
+            this.kryptonPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private ComponentFactory.Krypton.Toolkit.KryptonPanel kryptonPanel2;
+        private System.Windows.Forms.Label Lbl_Cvp5;
+        private System.Windows.Forms.Label Lbl_Cvp4;
+        private System.Windows.Forms.Label Lbl_Cvp3;
+        private System.Windows.Forms.Label Lbl_Cvp2;
+        private System.Windows.Forms.Label Lbl_Cvp1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox Lbl_Soru;
+        private System.Windows.Forms.Label Lbl_AdminOnay;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ user/TeacherForm/TeacherOwnQuestionForm.cs
@@ -0,0 +1,55 @@
+﻿using System.Drawing;
+using ComponentFactory.Krypton.Toolkit;
+using Question_Answer.Questions;
+
+namespace Question_Answer.user.TeacherForm
+{
+    public partial class TeacherOwnQuestionForm : KryptonForm
+    {
+        private readonly Question _question;
+        public TeacherOwnQuestionForm(Question question)
+        {
+            InitializeComponent();
+            this._question = question;
+        }
+
+        private void TeacherOwnQuestionForm_Load(object sender, System.EventArgs e)
+        {
+            Lbl_Soru.Text = _question.QuestionText;
+            if (_question.Answers[0].TrueOrFalse)
+                Lbl_Cvp1.BackColor = Color.GreenYellow;
+            else if (_question.Answers[1].TrueOrFalse)
+                Lbl_Cvp2.BackColor = Color.GreenYellow;
+            else if (_question.Answers[2].TrueOrFalse)
+                Lbl_Cvp3.BackColor = Color.GreenYellow;
+            else if (_question.Answers[3].TrueOrFalse)
+                Lbl_Cvp4.BackColor = Color.GreenYellow;
+            else if (_question.Answers[4].TrueOrFalse) 
+                Lbl_Cvp5.BackColor = Color.GreenYellow;
+
+
+            Lbl_Cvp1.Text = _question.Answers[0].AnswerText;
+            Lbl_Cvp2.Text = _question.Answers[1].AnswerText;
+            Lbl_Cvp3.Text = _question.Answers[2].AnswerText;
+            Lbl_Cvp4.Text = _question.Answers[3].AnswerText;
+            Lbl_Cvp5.Text = _question.Answers[4].AnswerText;
+            if (_question.AdminAccept)
+            {
+                Lbl_AdminOnay.Text = "Onaylandı";
+                Lbl_AdminOnay.ForeColor = Color.GreenYellow;
+            }
+            else
+            {
+                Lbl_AdminOnay.Text = "Onaylanmadı";
+                Lbl_AdminOnay.ForeColor = Color.Red;
+            }
+
+            if (_question.QuestionImage != "")
+            {
+                pictureBox1.ImageLocation = _question.QuestionImage != null
+                    ? _question.QuestionImagesUploadLocation + @"\" + _question.QuestionImage
+                    : null;
+            }
+        }
+    }
+}
