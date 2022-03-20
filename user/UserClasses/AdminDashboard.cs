43b96b9ab2052efbca5b511ef266b48b45ea7bd4
mustafa uçar <mustafa_asus@hotmail.com>
2022-03-21 02:28:14 +0300

    Add admin dashboard class

---
+++ user/UserClasses/AdminDashboard.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using MongoDB.Bson;
+using Question_Answer.Questions;
+
+namespace Question_Answer.user.UserClasses
+{
+    internal class AdminDashboard
+    {
+
+        private Database.MongoDB mongoDb;
+
+        public AdminDashboard()
+        {
+            mongoDb = new Database.MongoDB();
+        }
+
+        public List<Student> GetAllStudents()
+        {
+            var collection = mongoDb.LoadRecords<Student>("Students");
+            return collection;
+        }
+        public List<Teacher> GetAllTeachers()
+        {
+            var collection = mongoDb.LoadRecords<Teacher>("Teachers");
+            return collection;
+        }
+
+        public List<Question> GetAllUnconfirmendQuestions()
+        {
+            var collection = mongoDb.LoadRecordByFilter<Question>("Questions", new BsonDocument
+            {
+                { "AdminAccept", false }
+            });
+        return collection;
+        }
+
+        public List<Question> GetAllConfirmendQuestions()
+        {
+            var collection = mongoDb.LoadRecords<Question>("Confirmed_questions");
+            return collection;
+        }
+
+        public void ConfirmQuestion(Question question)
+        {
+            question.AdminAccept = true;
+            mongoDb.UpdateRecord("Questions",question.QuestionId,question);
+            mongoDb.InsertRecord("Confirmed_questions", question);
+        }
+
+
+    }
+}
