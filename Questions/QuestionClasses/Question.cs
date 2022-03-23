8f55f30c57ad260ea8a753d96070bd184b98f8b0
mustafa uçar <mustafa_asus@hotmail.com>
2022-03-23 21:31:07 +0300

    Add get question by day

--- Questions/QuestionClasses/Question.cs
+++ Questions/QuestionClasses/Question.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Windows.Forms;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Driver;
+using Question_Answer.user.UserClasses;
 
 namespace Question_Answer.Questions
 {
@@ -54,15 +56,56 @@ namespace Question_Answer.Questions
             mongoDB.InsertRecord("Questions", this);
         }
 
-        public void UpdateImage()
+        public void Update()
         {
             mongoDB.UpdateRecord("Questions", this.QuestionId, this);
         }
 
-        public List<Question> Rastgele10SoruGetir()
+        public bool SameQuestionControl(List<Question> list1,List<Question> list2)
+        {
+            var AllGetQuestionList = new List<Question>();
+            foreach (var random10Qeustion in list1)
+            {
+                AllGetQuestionList.Add(random10Qeustion);   
+            }
+
+            foreach (var question in list2)
+            {
+                AllGetQuestionList.Add(question);
+            }
+
+            AllGetQuestionList.Distinct();
+            if (AllGetQuestionList.Count != list1.Count + list2.Count)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+
+        public List<Question> GetRandom10Qeustions()
         {
             var questions = mongoDB.LoadRecordRandomQuestion();
             return questions.ToList();
+        }
+
+        public List<Question> GetQuestionsbyDay(Student student)
+        {
+            var QuestionbyDay = new List<Question>();
+            foreach (var answeredQuestion in student.AnsweredQuestionsList)
+            {
+                if (answeredQuestion.TrueOrFalse)
+                {
+                    var day = (DateTime.Now - answeredQuestion.AnsweredQuestionDate).Days;
+                    if (day is 1 or 7 or 30 or 180 or 365)
+                    {
+                        QuestionbyDay.Add(mongoDB.LoadRecordById<Question>("Confirmed_questions",answeredQuestion.AnsweredQuestionIds));
+                    }
+                }
+            }
+
+            return QuestionbyDay;
         }
 
     }
